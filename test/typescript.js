6d824eeb49563b2fb38668c7418d3c76db94c7e3
Martin Staffa <mjstaffa@gmail.com>
2021-12-19 20:56:56 +0100

    fix: improve typescript parsing (#9)
    * fix: improve typescript parsing
    
    Ignore nodes that acorn-walk doesn't support.
    This should not affect the extraction, because
    test suites are built only from CallExpressions.
    
    Fixes #8
    
    * test: it inside ts class method with modifier

--- test/typescript.js
+++ test/typescript.js
@@ -2,32 +2,7 @@ const { stripIndent } = require('common-tags')
 const test = require('ava')
 const { getTestNames } = require('..')
 
-test('typescript', (t) => {
-  t.plan(1)
-  const source = stripIndent`
-    describe('foo', () => {
-      it('bar', () => {
-        const typed: string = 'abc';
-      })
-    })
-  `
-  const result = getTestNames(source)
-
-  t.deepEqual(result, {
-    suiteNames: ['foo'],
-    testNames: ['bar'],
-    tests: [
-      {
-        name: 'bar',
-      },
-      {
-        name: 'foo',
-      },
-    ],
-  })
-})
-
-test('typescript 2', (t) => {
+test('typescript annotation', (t) => {
   t.plan(1)
   const source = stripIndent`
     describe('TypeScript spec', () => {
@@ -64,38 +39,121 @@ test('typescript 2', (t) => {
   })
 })
 
-// SKIP: https://github.com/bahmutov/find-test-names/issues/8
-test.skip('typescript interface', (t) => {
+test('typescript interface', (t) => {
   t.plan(1)
   const source = stripIndent`
     interface Person {
       name: string
     }
+
+    describe('TypeScript spec', () => {
+      it('works', () => {})
+    })
   `
   const result = getTestNames(source)
 
-  t.deepEqual(result, {})
+  t.deepEqual(result, {
+    suiteNames: ['TypeScript spec'],
+    testNames: ['works'],
+    tests: [
+      {
+        name: 'works',
+      },
+      {
+        name: 'TypeScript spec',
+      },
+    ],
+  })
 })
 
-// SKIP: https://github.com/bahmutov/find-test-names/issues/8
-test.skip('typescript type', (t) => {
+test('typescript type', (t) => {
   t.plan(1)
   const source = stripIndent`
     type Person = {
       name: string
     }
+
+    describe('TypeScript spec', () => {
+      it('works', () => {})
+    })
   `
   const result = getTestNames(source)
 
   t.deepEqual(result, {
-    suiteNames: ['foo'],
-    testNames: ['bar'],
+    suiteNames: ['TypeScript spec'],
+    testNames: ['works'],
     tests: [
       {
-        name: 'bar',
+        name: 'works',
       },
       {
-        name: 'foo',
+        name: 'TypeScript spec',
+      },
+    ],
+  })
+})
+
+test('typescript enum', (t) => {
+  t.plan(1)
+  const source = stripIndent`
+    enum Person {
+      name = 'name',
+      age = 'age'
+    }
+
+    describe('TypeScript spec', () => {
+      it('works', () => {})
+    })
+  `
+  const result = getTestNames(source)
+
+  t.deepEqual(result, {
+    suiteNames: ['TypeScript spec'],
+    testNames: ['works'],
+    tests: [
+      {
+        name: 'works',
+      },
+      {
+        name: 'TypeScript spec',
+      },
+    ],
+  })
+})
+
+test('typescript class with field modifiers', (t) => {
+  t.plan(1)
+  const source = stripIndent`
+    class Test {
+      private foo = 'private'
+
+      public bar = 'public'
+
+      protected baz = 'protected'
+
+      public createSuite() {
+        it('loads', () => {})
+      }
+    }
+
+    describe('TypeScript spec', () => {
+      it('works', () => {})
+    })
+  `
+  const result = getTestNames(source)
+
+  t.deepEqual(result, {
+    suiteNames: ['TypeScript spec'],
+    testNames: ['loads', 'works'],
+    tests: [
+      {
+        name: 'loads',
+      },
+      {
+        name: 'works',
+      },
+      {
+        name: 'TypeScript spec',
       },
     ],
   })
