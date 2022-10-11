bcf7d6b795e74a61a5777729089b52ad6e19470f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-10-11 12:55:29 -0400

    feat: add findEffectiveTestTagsIn

--- test/find-effective-tags.js
+++ test/find-effective-tags.js
@@ -1,5 +1,6 @@
-const { findEffectiveTestTags } = require('../src')
+const { findEffectiveTestTags, findEffectiveTestTagsIn } = require('../src')
 const { stripIndent } = require('common-tags')
+const { join } = require('path')
 const test = require('ava')
 
 test('finds effective test tags for each test', (t) => {
@@ -20,6 +21,17 @@ test('finds effective test tags for each test', (t) => {
     'parent child works a': ['@auth', '@one', '@user'],
     'parent child works b': ['@auth', '@user'],
     'has no tags': [],
+  }
+  t.deepEqual(result, expected)
+})
+
+test('finds effective test tags in a file', (t) => {
+  t.plan(1)
+  const specFilename = join(__dirname, '..', 'test-cy', 'spec-a.js')
+  const result = findEffectiveTestTagsIn(specFilename)
+  const expected = {
+    'Suite A works 1': [],
+    'Suite A works 2': ['A'],
   }
   t.deepEqual(result, expected)
 })
