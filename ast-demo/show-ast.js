f15a6f70d19da074288c8e5aa5dc5f041eb6996b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-07 14:48:34 -0500

    look at the comments node

---
+++ ast-demo/show-ast.js
@@ -0,0 +1,22 @@
+const babel = require('@babel/parser')
+
+const source = `
+  /**
+   * beginning
+   */
+
+  // this is a comment
+
+  const foo = 'bar'
+`
+
+const plugins = [
+  'estree', // To generate estree compatible AST
+]
+
+const AST = babel.parse(source, {
+  plugins,
+  sourceType: 'script',
+}).program
+
+console.log(AST.body[0].leadingComments)
